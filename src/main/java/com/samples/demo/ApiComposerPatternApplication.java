35964825718bf419264a30726acae256630df452
ekram <ekramalikazi@gmail.com>
2022-07-08 15:13:34 -0700

    initial commit for api composer

---
+++ src/main/java/com/samples/demo/ApiComposerPatternApplication.java
@@ -0,0 +1,13 @@
+package com.samples.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComposerPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComposerPatternApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/samples/demo/config/DatabaseConfig.java
@@ -0,0 +1,46 @@
+package com.samples.demo.config;
+
+import io.r2dbc.h2.H2ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.transaction.ReactiveTransactionManager;
+
+@Configuration
+@EnableR2dbcRepositories
+public class DatabaseConfig extends AbstractR2dbcConfiguration {
+
+
+    @Bean
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
+    }
+
+
+    @Override
+    public ConnectionFactory connectionFactory() {
+        return H2ConnectionFactory.inMemory("testdb");
+    }
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
+    }
+
+}
